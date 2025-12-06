b373d9b75f5ef87972b61a84fd60829ff8367e90
lnpbqc <1264776510@qq.com>
2025-12-06 23:41:49 +0800

    不知道干了啥

--- app/api/notes/[id]/route.ts
+++ app/api/notes/[id]/route.ts
@@ -1,8 +1,6 @@
-import { NextResponse } from "next/server";
+import {NextRequest, NextResponse} from "next/server";
 // todo: 笔记分表
-export async function GET({ params }: { params: { id: string } }) {
-    console.log("params:", params); // { id: '1' }
-
-    const id = Number(params.id); // 转成数字
+export async function GET(request:NextRequest,{ params }: { params: Promise<{ id: string }> }) {
+    const {id} = await params;
     return NextResponse.json({ success: true, noteId: id });
 }
